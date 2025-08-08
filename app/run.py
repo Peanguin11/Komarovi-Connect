04f554063f1bc85365d130286cf3c9a40eb959a1
Saba Iluridze <iluridzesaba11@gmail.com>
2025-08-08 20:29:48 +0400

    FIxed links, created basic flask code

---
+++ app/run.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+@app.route("/news")
+def news():
+    return render_template('news.html')
+
+@app.route("/events")
+def events():
+    return render_template('events.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('donations.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
